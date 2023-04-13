f89ee7a8fc35413b1f67e63e9ba56e65876090a1
Fayza Hamada <FayzaHamada00@gmail.com>
2023-04-13 05:13:00 -0700

    Delete reverse.pl

--- reverse.pl
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/perl
-print "Enter some lines, then press Ctrl-D to excit:\n"; 
-# or maybe Ctrl-Z 
-@lines = <STDIN>; 
-@reverse_lines = reverse @lines; 
-print ("your reverse is\n @reverse_lines");
